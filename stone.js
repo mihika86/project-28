fb51b139a20f7c23839233d663262832599c375a
mihika86 <82373671+mihika86@users.noreply.github.com>
2021-08-19 14:35:36 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -11,15 +11,13 @@ class Stone
 		this.x=x;
 		this.y=y;
 		this.r=r
-		this.image=loadImage("images/stone.png");
+		this.image=loadImage("stone.png");
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
 		World.add(world, this.body);
 
 	}
 
-	fly() {
-		 this.launcher.body = null
-	}
+	
 	display(){
 		
 			var stonepos=this.body.position;		
